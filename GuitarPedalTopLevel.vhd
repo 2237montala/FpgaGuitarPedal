a881d2f0167d8eba6be3399b1e1b8c6efccd0f2b
2237montala <anthony.montalbano@marquette.edu>
2021-04-08 12:22:22 -0500

    Added gitignore

--- GuitarPedalTopLevel.vhd
+++ GuitarPedalTopLevel.vhd
@@ -25,6 +25,52 @@ end GuitarPedalTopLevel;
 
 architecture my_structure of GuitarPedalTopLevel is
 
+COMPONENT audioInTest
+	port (
+		reg_clk     : in  std_logic                     := '0';             --       control_clock.clk
+		reg_reset   : in  std_logic                     := '0';             -- control_clock_reset.reset
+		aes_clk     : in  std_logic                     := '0';             --   conduit_aes_audio.export
+		aes_de      : in  std_logic                     := '0';             --                    .export
+		aes_ws      : in  std_logic                     := '0';             --                    .export
+		aes_data    : in  std_logic                     := '0';             --                    .export
+		aud_clk     : in  std_logic                     := '0';             --          dout_clock.clk
+		reset       : in  std_logic                     := '0';             --    dout_clock_reset.reset
+		aud_ready   : in  std_logic                     := '0';             --                dout.ready
+		aud_valid   : out std_logic;                                        --                    .valid
+		aud_sop     : out std_logic;                                        --                    .startofpacket
+		aud_eop     : out std_logic;                                        --                    .endofpacket
+		aud_channel : out std_logic_vector(7 downto 0);                     --                    .channel
+		aud_data    : out std_logic_vector(23 downto 0);                    --                    .data
+		channel0    : in  std_logic_vector(7 downto 0)  := (others => '0'); --     conduit_control.export
+		channel1    : in  std_logic_vector(7 downto 0)  := (others => '0'); --                    .export
+		fifo_status : out std_logic_vector(7 downto 0);                     --                    .export
+		fifo_reset  : in  std_logic                     := '0'              --                    .export
+	);
+end COMPONENT;
+
+COMPONENT outAudioTest
+	port (
+		reg_clk     : in  std_logic                     := '0';             --       control_clock.clk
+		reg_reset   : in  std_logic                     := '0';             -- control_clock_reset.reset
+		aud_clk     : in  std_logic                     := '0';             --           din_clock.clk
+		reset       : in  std_logic                     := '0';             --     din_clock_reset.reset
+		aud_ready   : out std_logic;                                        --                 din.ready
+		aud_valid   : in  std_logic                     := '0';             --                    .valid
+		aud_sop     : in  std_logic                     := '0';             --                    .startofpacket
+		aud_eop     : in  std_logic                     := '0';             --                    .endofpacket
+		aud_channel : in  std_logic_vector(7 downto 0)  := (others => '0'); --                    .channel
+		aud_data    : in  std_logic_vector(23 downto 0) := (others => '0'); --                    .data
+		aes_clk     : in  std_logic                     := '0';             --   conduit_aes_audio.export
+		aes_de      : out std_logic;                                        --                    .export
+		aes_ws      : out std_logic;                                        --                    .export
+		aes_data    : out std_logic;                                        --                    .export
+		channel0    : in  std_logic_vector(7 downto 0)  := (others => '0'); --     conduit_control.export
+		channel1    : in  std_logic_vector(7 downto 0)  := (others => '0'); --                    .export
+		fifo_status : out std_logic_vector(7 downto 0);                     --                    .export
+		fifo_reset  : in  std_logic                     := '0'              --                    .export
+	);
+end COMPONENT;
+
 component volume_control is
 
 
